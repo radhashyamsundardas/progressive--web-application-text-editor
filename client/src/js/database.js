0f13cb0f56fd2db6cc5e07140919716e24e70148
hiren patel <hirenpatel0526@yahoo.com>
2022-11-10 19:24:24 -0800

    done with adding database

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,9 +13,41 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (id, value) =>{
+  
+  console.error('put request to update jatedb');
+
+  const jateDb = await openDB('jate', 1);
+
+  const tx = jateDb.transaction('jate','readWrite');
+
+  const store = tx.objectStore('jate');
+
+  const request = objectStore.getAll()
+
+  const res = await request;
+  console.log('data saved to jatedb', res);
+
+
+};
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async (value) =>{
+
+  console.error('getting Database from JATE');
+
+  const jateDb = await openDB('jate', 1);
+
+  const tx = jateDb.transaction('jate', 'readWrite');
+
+  const store = tx.objectStore('jate');
+
+  const request = store.getAll()
+
+  const res = await request;
+  console.log('date saved to jatedb', res);
+};
+
+
 
 initdb();
