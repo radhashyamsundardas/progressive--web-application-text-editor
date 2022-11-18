842694ae316b2af235684b3f2a49a64dc52e5ce1
hiren patel <hirenpatel0526@yahoo.com>
2022-11-17 19:50:44 -0800

    changed web pack

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -7,6 +7,69 @@ const { verify } = require('crypto');
 // TODO: Add and configure workbox plugins for a service worker and manifest file.
 // TODO: Add CSS loaders and babel to webpack.
 
+// module.exports = () => {
+//   return {
+//     mode: 'development',
+//     entry: {
+//       main: './src/js/index.js',
+//       install: './src/js/install.js'
+//     },
+//     output: {
+//       filename: '[name].bundle.js',
+//       path: path.resolve(__dirname, 'dist'),
+//     },
+//     plugins: [
+//       new HtmlWebpackPlugin({
+//         template: './index.html',
+//         title: 'pwa editor'
+//       }),
+//       new InjectManifest({
+//         swSrc: './src-sw.js',
+//         swDest: '.src-sw.js'
+//       }),
+//       new WebpackPwaManifest({
+//         fingerprints: false,
+//         inject: true,
+//         name: 'textEditor',
+//         short_name: 'JATE',
+//         description: 'Just another text editor',
+//         background_color: '#225ca3',
+//         theme_color: '#225ca3',
+//         start_url: './',
+//         publicPath: './',
+//         icons: [
+//           {
+//             src: path.resolve('src/images/logo.png'),
+//             sizes: [96, 128, 192, 256, 384, 512 ],
+//             destination: path.join('assets', 'icons'),
+//           },
+//         ],
+//       }),
+//     ],
+
+//     module: {
+//       rules: [
+//         {
+//           test: /\.css$/,
+//           use: ['style-loader', 'css-loader'],
+//       },
+//       {
+//         test: /\.m?js$/,
+//         exclude: /node_modules/,
+
+//         use: {
+//           loader: 'babel-loader',
+//           options: {
+//             presets: ['@babel/preset-env'],
+//             plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],
+//           },
+//         },
+//       },
+//       ],
+//     },
+//   };
+// };
+
 module.exports = () => {
   return {
     mode: 'development',
@@ -20,50 +83,43 @@ module.exports = () => {
     },
     plugins: [
       new HtmlWebpackPlugin({
-        template: './index.html',
-        title: 'JATE'
+        template: "./index.html",
+        title: "pwa editor",
       }),
       new InjectManifest({
-        swSrc: './src-sw.js',
-        swDest: '.src-sw.js'
+        swSrc: "./src-sw.js",
+        swDest: "src-sw.js",
       }),
       new WebpackPwaManifest({
         fingerprints: false,
         inject: true,
-        name: 'text-editor',
-        short_name: 'JATE',
-        description: 'Just another text editor',
-        background_color: '#225ca3',
-        theme_color: '#225ca3',
-        start_url: './',
-        publicPath: './',
-        icons: [
-          {
-            src: path.resolve('src/images/logo.png'),
-            sizes: [96, 128, 192, 256, 384, 512 ],
-            destination: path.join('assets', 'icons'),
-          },
-        ],
-      }),
+        name: "textEditor",
+        short_name: "Jate",
+        description: "This is a notetaker for js",
+        id: "/",
+        start_url: "/",
+        publicPath: "/",
+      })
     ],
-
     module: {
       rules: [
         {
-          test: /\.css$/,
+          test: /\.css$/i,
           use: ['style-loader', 'css-loader'],
       },
       {
-        test: /\.m?js$/,
-        exclude: /node_modules/,
-
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env'],
-            plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          use: {
+              loader: 'babel-loader',
+              options: {
+                  presets: ['@babel/preset-env'],
+                  plugins: [
+                      '@babel/plugin-proposal-object-rest-spread',
+                      '@babel/transform-runtime',
+                  ],
+              },
           },
-        },
       },
       ],
     },
